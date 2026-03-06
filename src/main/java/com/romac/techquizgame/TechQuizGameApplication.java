af76b0bfb58926165996cb4545d15ef0f0848052
David-Romak <kizitodavid18@gmail.com>
2026-03-06 09:11:50 +0300

    initial commit

---
+++ src/main/java/com/romac/techquizgame/TechQuizGameApplication.java
@@ -0,0 +1,12 @@
+package com.romac.techquizgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechQuizGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechQuizGameApplication.class, args);
+    }
+}
---
+++ src/test/java/com/romac/techquizgame/TechQuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romac.techquizgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechQuizGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
